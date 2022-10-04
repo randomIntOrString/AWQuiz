e16210836ef6c9fbb1793ac582018cb83fd4367d
Simon <Simon.Henriks@awacademy.training>
2022-10-04 16:39:31 +0200

    La till kommentar med projektbeskrivning.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,3 +7,16 @@ public class Main {
         System.out.println("Lets Go!");
     }
 }
+
+
+//Frågesport
+//
+//- Textbox med frågor.
+//- helst knappar (tänk Postkodsmiljonären) (så som vi gjorde quit-funktionen)
+//- Bakgrund som visar rött (och gärna gör ljud) om det är fel svar.
+//- Bakgrund som visar grön (och gärna gör ljud) om det är korrekt svar.
+//- Score, både visuellt och i text. Säg att det är 10 frågor, vid start är de neutrala, och sen blir
+//1/10 grön eller röd beroende på svar.
+//- I slutet så sammanställer vi resultatet.
+//- Spelaren får ange sitt namn (som sen presenteras i slutet).
+//- (om tid finns, spela igen-funktion eftersom att vi inte kan databaser - än! :D )
