7306989da768605aa8cab4d834660b5cd09d50a0
Simon <Simon.Henriks@awacademy.training>
2022-10-05 10:00:50 +0200

    Uppdatering

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,60 @@
 package org.example;
 
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) throws Exception{
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
-        Terminal terminal = terminalFactory.createTerminal();
+        TerminalSize ts = new TerminalSize(70, 20);
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+        defaultTerminalFactory.setInitialTerminalSize(ts);
+        Terminal terminal = defaultTerminalFactory.createTerminal();
         terminal.setCursorVisible(false);
+
+        boolean continueReadingInput = true;
+        while (continueReadingInput) {
+            KeyStroke keyStroke = null;
+
+            do {
+                keyStroke.terminal.
+            }
+        }
+
+        String svarsalternativ1 = "Ruta ett";
+        String svarslaternativ2 = "Ruta två";
+        String svarsalternativ3 = "Ruta tre";
+
+
+
+        for (int i = 0; i < svarsalternativ1.length(); i++) {
+            terminal.setCursorPosition(2 + i, 17);
+            terminal.putCharacter(svarsalternativ1.charAt(i));
+        }
+
+        for (int i = 0; i < svarslaternativ2.length(); i++) {
+            terminal.setCursorPosition(25 + i, 17);
+
+            terminal.putCharacter(svarslaternativ2.charAt(i));
+        }
+
+        for (int i = 0; i < svarsalternativ3.length(); i++) {
+            terminal.setCursorPosition(50 + i, 17);
+            terminal.putCharacter(svarsalternativ3.charAt(i));
+        }
 
         Questions[] questions = new Questions[2];
 
         questions[0] = new Questions("Fråga 1", "Svar A", "Svar B", "Svar C", Answer.C);
         questions[1] = new Questions("Fråga 2", "Svar A", "Svar B", "Svar C", Answer.B);
+    }
+
+    public static void getQuestions(Questions[] questions){
+        for (int i = 0; i < questions.length; i++) {
+
+        }
     }
 }
 
